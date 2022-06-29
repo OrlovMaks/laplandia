ef6279fae76e787cb681fbad23e5d463bef99c59
OrlovMaks <4014499@gmail.com>
2022-06-29 20:25:17 +0300

    fix()

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -1,4 +1,19 @@
-// Импорт функционала ==============================================================================================================================================================================================================================================================================================================================
-// import { isMobile } from "./functions.js";
-// import { formsModules } from "./forms/forms.js";
+import { Fancybox } from "@fancyapps/ui";
+import "@fancyapps/ui/dist/fancybox.css";
 
+window.onload = function () {
+    const $links = document.querySelectorAll('.header__link');
+
+    $links.forEach(element => {
+        if (element.href === document.URL) element.classList.add('_active');
+    });
+
+    // setTimeout(() => {
+    document.body.classList.add('loaded_hiding');
+    // }, 1500)
+
+    window.setTimeout(() => {
+        document.body.classList.add('loaded');
+        document.body.classList.remove('loaded_hiding');
+    }, 1500);
+}
--- src/js/files/sliders.js
+++ src/js/files/sliders.js
@@ -7,7 +7,7 @@
 // Подключаем слайдер Swiper из node_modules
 // При необходимости подключаем дополнительные модули слайдера, указывая их в {} через запятую
 // Пример: { Navigation, Autoplay }
-import Swiper, { Navigation } from 'swiper';
+import Swiper, { Navigation, Autoplay } from 'swiper';
 /*
 Основниые модули слайдера:
 Navigation, Pagination, Autoplay, 
@@ -49,14 +49,13 @@ function initSliders() {
 		new Swiper('.swiper', {
 			// Подключаем модули слайдера
 			// для конкретного случая
-			//modules: [Navigation, Pagination],
-			/*
-			effect: 'fade',
+			modules: [Navigation, Autoplay],
+
+			// effect: 'fade',
 			autoplay: {
-				delay: 3000,
+				delay: 1500,
 				disableOnInteraction: false,
 			},
-			*/
 			observer: true,
 			observeParents: true,
 			slidesPerView: 1,
@@ -65,14 +64,14 @@ function initSliders() {
 			speed: 800,
 			//touchRatio: 0,
 			//simulateTouch: false,
-			//loop: true,
+			loop: true,
 			//preloadImages: false,
 			//lazy: true,
 			// Dotts
-			//pagination: {
-			//	el: '.slider-quality__pagging',
-			//	clickable: true,
-			//},
+			// pagination: {
+			// 	el: '.slider-quality__pagging',
+			// 	clickable: true,
+			// },
 			// Arrows
 			navigation: {
 				nextEl: '.about__more .more__item_next',
