4543a0a1bc23f9d1d01e7ab07928f418179acc0b
OrlovMaks <4014499@gmail.com>
2022-06-30 15:25:58 +0300

    ui fixes

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -4,16 +4,47 @@ import "@fancyapps/ui/dist/fancybox.css";
 window.onload = function () {
     const $links = document.querySelectorAll('.header__link');
 
+
     $links.forEach(element => {
         if (element.href === document.URL) element.classList.add('_active');
     });
 
-    // setTimeout(() => {
-    document.body.classList.add('loaded_hiding');
-    // }, 1500)
+    let params = (new URL(document.location)).searchParams;
+    const isScroll = params.get("scroll")
+    if (isScroll) {
+        const block = document.getElementById(`${isScroll}`)
+        window.scroll(0, block.offsetTop)
+    }
+}
 
-    window.setTimeout(() => {
-        document.body.classList.add('loaded');
-        document.body.classList.remove('loaded_hiding');
-    }, 1500);
+let isMobile = {
+    Android: function () { return navigator.userAgent.match(/Android/i); },
+    BlackBerry: function () { return navigator.userAgent.match(/BlackBerry/i); },
+    iOS: function () { return navigator.userAgent.match(/iPhone|iPad|iPod/i); },
+    Opera: function () { return navigator.userAgent.match(/Opera Mini/i); },
+    Windows: function () { return navigator.userAgent.match(/IEMobile/i); },
+    any: function () { return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows()); }
+};
+let body = document.querySelector('body');
+if (isMobile.any()) {
+    body.classList.add('touch');
+    let arrow = document.querySelectorAll('.arrow');
+    const headerDropdownLink = document.querySelector('.header__dropdown-link')
+    for (let i = 0; i < arrow.length; i++) {
+        let thisLink = arrow[i].previousElementSibling;
+        let subMenu = arrow[i].nextElementSibling;
+        let thisArrow = arrow[i];
+
+        thisLink.classList.add('parent');
+        arrow[i].addEventListener('click', function () {
+            subMenu.classList.toggle('open');
+            thisArrow.classList.toggle('active');
+        });
+        headerDropdownLink.addEventListener('click', function () {
+            subMenu.classList.toggle('open');
+            thisArrow.classList.toggle('active');
+        });
+    }
+} else {
+    body.classList.add('mouse');
 }
